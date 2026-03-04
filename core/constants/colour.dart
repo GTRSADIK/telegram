f3d0b3050039124edc599ec04f057b8391b7710b
GTRSADIK <info.gtrbusiness@gmail.com>
2026-03-04 09:49:36 +0600

    Create colour.dart

---
+++ core/constants/colour.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class TgColors {
+  static const bg = Color(0xFF0F1720);
+  static const appBar = Color(0xFF17212B);
+  static const bubbleMe = Color(0xFF2AABEE);
+  static const bubbleOther = Color(0xFF182533);
+}
